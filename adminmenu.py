4660a18ffb3878e5262cd452e382f4f643145265
244sinfall <dimafilin6@icloud.com>
2021-11-05 01:57:57 +0700

    ура блять

--- adminmenu.py
+++ adminmenu.py
@@ -1,6 +1,7 @@
 import json
 
 from PyQt5 import QtGui, uic
+from PyQt5.QtGui import QIntValidator, QDoubleValidator
 from PyQt5.QtWidgets import QTableWidgetItem, QWidget
 
 import foodproducts.foodproduct
@@ -8,12 +9,14 @@ from foodproducts import foodproduct
 
 
 def load_products(window) -> None:
+    while window.productsTable.rowCount() > 0:
+        window.productsTable.removeRow(0)
     with open('foodproducts/products.json', 'r') as inputfile:
         row = 0
         counter = json.load(inputfile)
         for object_counter in counter:
-            window.productsTable.insertRow(row)
             product_info = foodproduct.get_product_info(object_counter['name'])
+            window.productsTable.insertRow(row)
             window.productsTable.setItem(row, 0, QTableWidgetItem(str(product_info['name'])))
             window.productsTable.setItem(row, 1, QTableWidgetItem(str(product_info['protein'])))
             window.productsTable.setItem(row, 2, QTableWidgetItem(str(product_info['fats'])))
@@ -24,38 +27,31 @@ def load_products(window) -> None:
 
 
 class ProductAddMenu(QWidget):
-    def __init__(self, parent=None):
-        super(ProductAddMenu, self).__init__(parent)
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
         uic.loadUi('adminmenu/addproduct.ui', self)
         self.cancelAdderButton.clicked.connect(self.close)
         self.addConfirmButton.clicked.connect(self.add_new_product)
+        self.proteinAdder.setValidator(QDoubleValidator(0, 100, 2))
+        self.fatsAdder.setValidator(QDoubleValidator(0, 100, 2))
+        self.carbohydratesAdder.setValidator(QDoubleValidator(0, 100, 2))
+        self.caloriesAdder.setValidator(QIntValidator(0, 3000))
+        self.massAdder.setValidator(QIntValidator(0, 1000))
+        self.priceAdder.setValidator(QDoubleValidator(0, 1000,2))
+
+
 
     def add_new_product(self) -> None:
         name = self.nameAdder.text()
-        protein = self.proteinAdder.text()
-        fats = self.fatsAdder.text()
-        carbohydrates = self.carbohydratesAdder.text()
-        calories = self.caloriesAdder.text()
-        mass = self.massAdder.text()
-        price = self.priceAdder.text()
-        if isValuesCorrect(name, protein, fats, carbohydrates, calories, mass, price):
-            foodproduct.create(name, protein, fats, carbohydrates, calories, mass, price)
-            load_products(admin_menu)
-            self.close()
-        else:
-            self.adderStatusLabel.setText('Некорректный формат!')
+        protein = float(self.proteinAdder.text())
+        fats = float(self.fatsAdder.text())
+        carbohydrates = float(self.carbohydratesAdder.text())
+        calories = int(self.caloriesAdder.text())
+        mass = int(self.massAdder.text())
+        price = float(self.priceAdder.text())
+        foodproduct.create(name, protein, fats, carbohydrates, calories, mass, price)
+        load_products(self.parent)
+        self.close()
 
-
-def isValuesCorrect(name, protein, fats, carbohydrates, calories, mass, price) -> str:
-    try:
-        str(name)
-        float(protein)
-        float(fats)
-        float(carbohydrates)
-        int(calories)
-        int(mass)
-        int(price)
-        return True
-    except:
-        return False
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter
 from tkinter import *
-from PyQt5 import uic
+from PyQt5 import uic, QtGui
 from PyQt5 import QtWidgets, QtCore
 import sys
 
@@ -14,39 +14,40 @@ class WelcomeMenu(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('mainmenu.ui', self)
+        self.admin_menu = None
         self.admin_Mode_Trigger.clicked.connect(self.go_admin_menu)
 
     def go_admin_menu(self):
-        admin_menu.show()
+        self.admin_menu = AdminMenu(parent=self)
+        self.admin_menu.show()
+        self.hide()
 
 
 class AdminMenu(QWidget):
-    def __init__(self, parent=None):
-        super(AdminMenu, self).__init__(parent)
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
         uic.loadUi('adminmenu.ui', self)
         header = self.productsTable.horizontalHeader()
-        self.addproducter = ProductAddMenu(self)
         header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
-        self.addProductButton.clicked.connect(self.addproducter.show)
+        self.addProductButton.clicked.connect(self.addProductButton_clicked)
+        self.addproducter = None
         self.adminExitButton.clicked.connect(self.close)
-        # header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
-        # header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
-        # header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
-        # header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
-        # header.setSectionResizeMode(5, QtWidgets.QHeaderView.Stretch)
-        # header.setSectionResizeMode(6, QtWidgets.QHeaderView.Stretch)
         load_products(self)
 
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        self.parent.show()
+        super().closeEvent(a0)
+
     def addProductButton_clicked(self) -> None:
-        addproducter.show()
+        self.addproducter = ProductAddMenu(parent=self)
+        self.addproducter.show()
 
 
 app = QtWidgets.QApplication(sys.argv)
 menu = WelcomeMenu()
 menu.show()
-admin_menu = AdminMenu()
-addproducter = ProductAddMenu()
 exit(app.exec_())
 
 
